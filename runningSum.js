2d2ae75b33874c3f2971e219de1cae0572123c66
igudy <goodnessigunma1@gmail.com>
2024-06-04 03:52:17 +0100

    runningSum

---
+++ runningSum.js
@@ -0,0 +1,30 @@
+// Question;
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+// ANSWER
+
+const runningSum = (nums) => {
+    let runningSums = [];
+
+    for(let i=0; i<nums.length; i++){
+        if(i === 0){
+            runningSums[i] = nums[i]
+        }
+        else{
+            runningSums[i] = runningSums[i-1] + nums[i]
+        }
+    }
+    return runningSums
+}
+
+let nums = [1, 2, 3, 4]
+console.log('running sum:', runningSum(nums));
+
